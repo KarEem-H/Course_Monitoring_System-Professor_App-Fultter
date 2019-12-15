2268b4e96913d840cbb07e99557d86b65968bcb1
unknown <alrsamkarim96@gmail.com>
2019-12-15 21:35:08 +0200

    FLUTTER : FIREBASE PUSH NOTIFICATION TUTORIAL| Firebase notify on Realtime

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,81 @@
 import 'package:flutter/material.dart';
 import 'Courses/CreateNewCourseForm.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+// ------------------------------ Notifications ----------------------------------------------------------
+
+  String textValue = 'Hello World !';
+  FirebaseMessaging firebaseMessaging = new FirebaseMessaging();
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      new FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+
+    var android = new AndroidInitializationSettings('mipmap/ic_launcher');
+    var ios = new IOSInitializationSettings();
+    var platform = new InitializationSettings(android, ios);
+    flutterLocalNotificationsPlugin.initialize(platform);
+
+    firebaseMessaging.configure(
+      onLaunch: (Map<String, dynamic> msg) {
+        print(" onLaunch called ${(msg)}");
+      },
+      onResume: (Map<String, dynamic> msg) {
+        print(" onResume called ${(msg)}");
+      },
+      onMessage: (Map<String, dynamic> msg) {
+        showNotification(msg);
+        print(" onMessage called ${(msg)}");
+      },
+    );
+    firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, alert: true, badge: true));
+    firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings setting) {
+      print('IOS Setting Registed');
+    });
+    firebaseMessaging.getToken().then((token) {
+      update(token);
+    });
+  }
+
+  showNotification(Map<String, dynamic> msg) async {
+    var android = new AndroidNotificationDetails(
+      'اهلا وسهلا',
+      "CHANNLE NAME",
+      "channelDescription",
+    );
+    var iOS = new IOSNotificationDetails();
+    var platform = new NotificationDetails(android, iOS);
+    await flutterLocalNotificationsPlugin.show(
+        0, "This is title", "this is demo", platform);
+  }
+
+  update(String token) {
+    print(token);
+    DatabaseReference databaseReference = new FirebaseDatabase().reference();
+    databaseReference.child('fcm-token/${token}').set({"token": token});
+    textValue = token;
+    setState(() {});
+  }
+// ----------------------------------------------------------------------------------------
+  @override
+   Widget build(BuildContext context) {
     final appTitle = 'Add a new course Form';
 
     return MaterialApp(
@@ -14,13 +84,36 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text(appTitle),
         ),
-        body: MyCustomForm(),
+        body: new Center(
+          child: new Column(
+            children: <Widget>[
+             MyCustomForm(),
+              new Text(
+                textValue,
+              )
+            ],
+          ),
+        ),        
       ),
     );
   }
+  // Widget build(BuildContext context) {
+  //   return new MaterialApp(
+  //     home: new Scaffold(
+  //       appBar: new AppBar(
+  //         title: new Text('Push Notification'),
+  //       ),
+  //       body: new Center(
+  //         child: new Column(
+  //           children: <Widget>[
+    //          MyCustomForm(),
+  //             new Text(
+  //               textValue,
+  //             )
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 }
-
-
-
-
-
